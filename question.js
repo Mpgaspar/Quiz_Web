daefc6bbbcbc67512837ebb7d5b492e9a506f120
Marcelo Gaspar <drmpgaspar@gmail.com>
2019-11-07 17:46:37 +0100

    Create an array of questions and answers

---
+++ question.js
@@ -0,0 +1,154 @@
+// HTML questions
+var questions = [{
+    "question": "Web pages are written using ?",
+    "option1": "FTP",
+    "option2": "HTTP",
+    "option3": "HTML",
+    "option4": "URL",
+    "answer": "3"
+}, {
+    "question": "Who is making the Web standards?",
+    "option1": "Mozilla",
+    "option2": "Microsoft",
+    "option3": "Google",
+    "option4": "The World Wide Web Consortium",
+    "answer": "4"
+}, {
+    "question": "Choose the correct HTML element for the largest heading:",
+    "option1": "<head>",
+    "option2": "<h1>",
+    "option3": "<heading>",
+    "option4": "<h6>",
+    "answer": "2"
+}, {
+    "question": "Choose the correct HTML element to define important text",
+    "option1": "<b>",
+    "option2": "<i>",
+    "option3": "<important>",
+    "option4": "<strong>",
+    "answer": "4"
+}, {
+    "question": "Which of these elements are all <table> elements?",
+    "option1": "<table><tr><td>",
+    "option2": "<table><head><tfoot>",
+    "option3": "<thead><body><tr>",
+    "option4": "<table><tr><tt>",
+    "answer": "1"
+}, {
+    "question": "What is the correct HTML for making a checkbox?",
+    "option1": "<checkbox>",
+    "option2": "<input type='check'>",
+    "option3": "<check>",
+    "option4": "<input type='checkbox'",
+    "answer": "4"
+}, {
+    "question": "In HTML, which attribute is used to specify that an input field must be filled out?",
+    "option1": "formvalidate",
+    "option2": "placeholder",
+    "option3": "required",
+    "option4": "validate",
+    "answer": "3"
+}, 
+// CSS questions
+{
+    "question": "What does CSS stand for?",
+    "option1": "Cascading Style Sheets",
+    "option2": "Computer Style Sheets",
+    "option3": "Colorful Style Sheets",
+    "option4": "Creative Style Sheets",
+    "answer": "1"
+}, {
+    "question": "Which HTML attribute is used to define inline styles?",
+    "option1": "font",
+    "option2": "class",
+    "option3": "style",
+    "option4": "styles",
+    "answer": "3"
+}, {
+    "question": "Which is the correct CSS syntax?",
+    "option1": "{body:color=black};",
+    "option2": "body:color=black;",
+    "option3": "{body;color:black;}",
+    "option4": "body{color:black;}",
+    "answer": "4"
+}, {
+    "question": "How do you insert a comment in a CSS file?",
+    "option1": "//this is a comment",
+    "option2": "/*this is a comment*/",
+    "option3": "'this is a comment",
+    "option4": "//this is a comment//",
+    "answer": "2"
+}, {
+    "question": "How do you select an element with id 'demo'?",
+    "option1": "#demo",
+    "option2": ".demo",
+    "option3": "*demo",
+    "option4": "demo",
+    "answer": "1"
+}, {
+    "question": "How do you select elements with class name 'test'?",
+    "option1": "*test",
+    "option2": ".test",
+    "option3": "#test",
+    "option4": "test",
+    "answer": "2"
+}, {
+    "question": "What is the default value of the position property?",
+    "option1": "absolute",
+    "option2": "fixed",
+    "option3": "relative",
+    "option4": "static",
+    "answer": "4"
+}, 
+// JavaScript questions
+{
+    "question": "What is the correct syntax for referring to an external script called 'function.js'?",
+    "option1": "<script href='function.js'>",
+    "option2": "<script src='function.js'>",
+    "option3": "<script name='function.js'>",
+    "option4": "<script link='function.js'>",
+    "answer": "2"
+}, {
+    "question": "How do you write 'Hello World' in an alert box?",
+    "option1": "alert('Hello World');",
+    "option2": "alertBox('Hello World');",
+    "option3": "msgBox('Hello World');",
+    "option4": "msg('Hello World');",
+    "answer": "1"
+}, {
+    "question": "How do you create a function in JavaScript?",
+    "option1": "function = myFunction()",
+    "option2": "function myFunction()",
+    "option3": "function:myFunction()",
+    "option4": "function(myFunction())",
+    "answer": "1"
+}, {
+    "question": "How do you call a function named 'myFunction'?",
+    "option1": "call function myFunction()",
+    "option2": "call myFunction()",
+    "option3": "myFunction()",
+    "option4": "call.myFunction()",
+    "answer": "3"
+}, {
+    "question": "What is the correct way to write a JavaScript array?",
+    "option1": "var colors = ['red','green','blue']",
+    "option2": "var colors = (1:'red',2:'green',3:'blue')",
+    "option3": "var colors = 1 =('red'),2 =('green'),3=('blue')",
+    "option4": "var colors = 'red','green','blue'",
+    "answer": "1"
+}, {
+    "question": "Which event occurs when the user clicks on an HTML element?",
+    "option1": "onmouseover",
+    "option2": "onchange",
+    "option3": "onmouseclick",
+    "option4": "onclick",
+    "answer": "4"
+}, {
+    "question": "Web pages are written using ?",
+    "option1": "FTP",
+    "option2": "HTTP",
+    "option3": "HTML",
+    "option4": "URL",
+    "answer": "3"
+}
+]
---
+++ quiz-script.js
@@ -0,0 +1,10 @@
+var currentQuestion = 0;
+var score = 0;
+
+var container = document.getElementById("quizContainer");
+var questionEl = document.getElementById("question");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var opt4 = document.getElementById("opt4");
+var totQuestions = questions.length;
